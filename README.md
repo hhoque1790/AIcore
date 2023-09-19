cdcaf413474be58338130419a92f3f18768b8b17
hhoque1790 <hhoque500@gmail.com>
2023-09-19 15:19:15 +0100

    2nd milestone

---
+++ README.md
@@ -0,0 +1,7 @@
+# Hangman Game
+## Contents
+## Description (what it does, the aim of the project, and what you learned)
+## Installation instructions
+## Usage instructions
+## File structure of the project
+## License information
--- milestone_2.py
+++ milestone_2.py
@@ -3,3 +3,9 @@ word_list=["apple","banana","cherry","orange","cucumber"]
 print(word_list)
 word=random.choice(word_list)
 print(word)
+
+guess=input("Please enter a letter: ")
+if len(guess)==1 and type(guess)==str:
+    print("good guess")
+else:
+    print("Oops! That is not a valid input.")
